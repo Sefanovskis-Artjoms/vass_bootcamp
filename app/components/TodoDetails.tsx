d9ce090cef2953464349a88e2b6e7d5573956fd3
Sefanovskis-Artjoms <artjoms200@gmail.com>
2024-11-25 17:11:17 +0200

    Reworked TodoDetails component for readability

--- app/components/TodoDetails.tsx
+++ app/components/TodoDetails.tsx
@@ -44,12 +44,59 @@ export default function TodoDetails({
     ? "UNASSIGNED"
     : `${assignedUser.name} ${assignedUser.surname} (${assignedUser.username})`;
 
-  return (
-    <div className="grid grid-cols-[160px_minmax(400px,600px)_120px] w-fit min-h-[150px] bg-gray-200 rounded-md shadow-md border border-slate-500 overflow-hidden">
-      <div className="flex flex-col justify-center m-3 border-r-2 border-slate-500">
-        <div className="mb-4">
-          <label className="text-slate-500 text-sm block ">Status:</label>
-          {isEditing ? (
+  // Component is basically split into two parts: one for viewing and one for editing
+  // This is made for readability
+  if (!isEditing) {
+    return (
+      <div className="grid grid-cols-[160px_minmax(400px,600px)_120px] w-fit min-h-[150px] bg-gray-200 rounded-md shadow-md border border-slate-500 overflow-hidden">
+        <div className="flex flex-col justify-center m-3 border-r-2 border-slate-500">
+          <div className="mb-4">
+            <label className="text-slate-500 text-sm block ">Status:</label>
+            <div className="text-xl font-semibold text-gray-800">
+              {information.status}
+            </div>
+          </div>
+          <div>
+            <label className="text-slate-500 text-sm block">Type:</label>
+            <div className="text-xl font-semibold text-gray-800">
+              {information.type}
+            </div>
+          </div>
+        </div>
+
+        <div className="p-4 flex flex-col">
+          <div className="mb-2">
+            <span className="font-bold text-xl text-gray-800">
+              {information.title}
+            </span>
+            <span className="text-sm text-slate-500">
+              , created on: {information.date}
+            </span>
+          </div>
+
+          <div className="mb-4 text-gray-700">{information.description}</div>
+
+          <div className="mt-auto">
+            <span className="text-sm text-slate-500">Assigned to: </span>
+            <span className="text-gray-900">{assignedTo}</span>
+          </div>
+        </div>
+
+        <button
+          className="flex items-center justify-center text-slate-400 hover:text-orange-600 hover:bg-gray-300 transition-colors duration-300"
+          onClick={() => setIsEditing(true)}
+        >
+          <PencilSquareIcon className="h-7 w-7 mr-1" />
+          <span className="text-md font-semibold">Edit</span>
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-[160px_minmax(400px,600px)_120px] w-fit min-h-[150px] bg-gray-200 rounded-md shadow-md border border-slate-500 overflow-hidden">
+        <div className="flex flex-col justify-center m-3 border-r-2 border-slate-500">
+          <div className="mb-4">
+            <label className="text-slate-500 text-sm block ">Status:</label>
             <select
               {...register("status")}
               className="text-xl font-semibold text-gray-800 bg-transparent border rounded border-gray-500"
@@ -58,15 +105,10 @@ export default function TodoDetails({
               <option value="Done">Done</option>
               <option value="To do">To do</option>
             </select>
-          ) : (
-            <div className="text-xl font-semibold text-gray-800">
-              {information.status}
-            </div>
-          )}
-        </div>
-        <div>
-          <label className="text-slate-500 text-sm block">Type:</label>
-          {isEditing ? (
+          </div>
+          <div>
+            <label className="text-slate-500 text-sm block">Type:</label>
+
             <select
               {...register("type")}
               className="text-xl font-semibold text-gray-800 bg-transparent border rounded border-gray-500"
@@ -76,55 +118,45 @@ export default function TodoDetails({
               <option value="Story">Story</option>
               <option value="Other">Other</option>
             </select>
-          ) : (
-            <div className="text-xl font-semibold text-gray-800">
-              {information.type}
-            </div>
-          )}
+          </div>
         </div>
-      </div>
 
-      <div className="p-4 flex flex-col">
-        <div className="mb-2">
-          <span className="font-bold text-xl text-gray-800">
-            {isEditing ? (
+        <div className="p-4 flex flex-col">
+          <div className="mb-2">
+            <span className="font-bold text-xl text-gray-800">
               <input
                 type="text"
                 {...register("title", { required: "Title is required" })}
                 className={`p-1 text-xl font-semibold bg-transparent border rounded border-gray-500
-                  ${errors.title ? "border-red-500" : "border-gray-300"}`}
+                    ${errors.title ? "border-red-500" : "border-gray-300"}`}
               />
-            ) : (
-              information.title
+            </span>
+            <span className="text-sm text-slate-500">
+              , created on: {information.date}
+            </span>
+            {errors.title && (
+              <p className="text-red-500 text-sm">{errors.title.message}</p>
             )}
-          </span>
-          <span className="text-sm text-slate-500">
-            , created on: {information.date}
-          </span>
-          {errors.title && (
-            <p className="text-red-500 text-sm">{errors.title.message}</p>
-          )}
-        </div>
-        <div className="mb-4 text-gray-700">
-          {isEditing ? (
+          </div>
+          <div className="mb-4 text-gray-700">
             <textarea
               {...register("description", {
                 required: "Description is required",
               })}
               className={`p-1 text-md bg-transparent border rounded border-gray-500 min-w-full min-h-[150px] 
-                 ${errors.description ? "border-red-500" : "border-gray-300"}`}
+                   ${
+                     errors.description ? "border-red-500" : "border-gray-300"
+                   }`}
             />
-          ) : (
-            information.description
-          )}
-          {errors.description && (
-            <p className="text-red-500 text-sm">{errors.description.message}</p>
-          )}
-        </div>
+            {errors.description && (
+              <p className="text-red-500 text-sm">
+                {errors.description.message}
+              </p>
+            )}
+          </div>
 
-        <div className="mt-auto">
-          <span className="text-sm text-slate-500">Assigned to: </span>
-          {isEditing ? (
+          <div className="mt-auto">
+            <span className="text-sm text-slate-500">Assigned to: </span>
             <select
               {...register("assignedTo")}
               id="assignToSelect"
@@ -141,13 +173,8 @@ export default function TodoDetails({
                 ))
               )}
             </select>
-          ) : (
-            <span className="text-gray-900">{assignedTo}</span>
-          )}
+          </div>
         </div>
-      </div>
-
-      {isEditing ? (
         <button
           className="flex items-center justify-center text-slate-400 hover:text-lime-700 hover:bg-gray-300 transition-colors duration-300"
           onClick={handleSubmit(onSubmit)}
@@ -155,15 +182,7 @@ export default function TodoDetails({
           <ClipboardDocumentCheckIcon className="h-7 w-7 mr-1" />
           <span className="text-md font-semibold">Save</span>
         </button>
-      ) : (
-        <button
-          className="flex items-center justify-center text-slate-400 hover:text-orange-600 hover:bg-gray-300 transition-colors duration-300"
-          onClick={() => setIsEditing(true)}
-        >
-          <PencilSquareIcon className="h-7 w-7 mr-1" />
-          <span className="text-md font-semibold">Edit</span>
-        </button>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
